d0d3e08b7f88839ce3e54f732e38ff667e4f1651
胡新敏 <huxinmin@qianmi.com>
2022-03-07 18:00:26 +0800

    init

--- src/git/getChanged.ts
+++ src/git/getChanged.ts
@@ -1,8 +1,20 @@
 import git from './';
 
-export default async ()=> {
+const getChanged =  async ()=> {
  const logs = await git.log({
-   "--pretty": "format: '%h - %an, %ad %cd : %s' ",
-   "--since" : "2022-01-02" // 或者2.weeks
+   "--since" : "2022-01-02" // 或者 2.weeks, 2 years 1 day 3 minutes ago, 2022-01-02
  })
+ const first = logs.all[logs.all.length - 1].hash;
+ const latest = logs.latest?.hash;
+ const diffRes = await git.diff({
+   `${first}`: `${latest}`,
+   "--stat": ""
+ }, (err)=> {
+    console.log(err)
+ });
+ console.log(diffRes)
 }
+
+getChanged()
+
+export default getChanged
