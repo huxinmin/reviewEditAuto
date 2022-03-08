bdb18767b8db62183d44161f5ab4310c12fa22c5
胡新敏 <huxinmin@qianmi.com>
2022-03-08 11:37:15 +0800

    test

--- src/git/getChanged.ts
+++ src/git/getChanged.ts
@@ -5,12 +5,12 @@ const getChanged =  async ()=> {
    "--since" : "2022-01-02" // 或者 2.weeks, 2 years 1 day 3 minutes ago, 2022-01-02
  })
  const first = logs.all[logs.all.length - 1].hash;
- const latest = logs.latest?.hash;
- const diffRes = await git.diff({
-   `${first}`: `${latest}`,
-   "--stat": ""
- }, (err)=> {
+ const latest = logs.latest?.hash || '';
+ const diffRes = await git.diffSummary([first, latest, '--stat'], (err)=> {
+  if (err) {
     console.log(err)
+    process.exit(1)
+  }
  });
  console.log(diffRes)
 }
